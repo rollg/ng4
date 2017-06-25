a921bc97c449e73d82b1f5f1eafd273487e560ca
Igor <maildevcom@gmail.com>
2017-06-25 10:33:22 +0300

    export lambda issue has been solved;

--- src/app/state-management/reducers/main-state.reducer.ts
+++ src/app/state-management/reducers/main-state.reducer.ts
@@ -2,15 +2,15 @@ import * as mainActions from '../actions/main-state.actions';
 import {initialState} from '../state';
 import {Counter} from '../../models';
 
-export const reducer = (state = initialState, action: mainActions.Actions): Counter => {
+export function  reducer (state = initialState, action: mainActions.Actions): Counter {
     switch (action.type) {
         case mainActions.INCREMENT:
             return {counter: state.counter + 1};
         case mainActions.SET_TO_CONST:
             return {counter: action.payload.constValue};
         case mainActions.DECREMENT:
-            return {counter: state.counter - 1};
+            return state.counter > 0 ? {counter: state.counter - 1} : {counter: 0};
         default:
             return state;
     }
-};
+}
